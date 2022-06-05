33939551d29228f108f7401824b5942437e2d2b2
prakalpxj <prakalp.jain.05@gmail.com>
2022-06-05 19:55:18 +0530

    added changes @CrossOrigin for API calling

--- EditorialBlog/src/main/java/com/deutsche/EditorialBlog/controller/UsersController.java
+++ EditorialBlog/src/main/java/com/deutsche/EditorialBlog/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.deutsche.EditorialBlog.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import com.deutsche.EditorialBlog.DAO.UsersDAO;
 import com.deutsche.EditorialBlog.bean.Users;
 import com.deutsche.EditorialBlog.model.UsersModel;
 import com.deutsche.EditorialBlog.repository.UsersRepository;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/EditorialBlog")
 
@@ -35,17 +36,17 @@ public class UsersController {
 	
 	
 	@GetMapping("login")
-	public ResponseEntity<Users> userLogin (@RequestParam("username") String username, @RequestParam("password") String password) {
+	public Users userLogin (@RequestParam("username") String username, @RequestParam("password") String password) {
 		Users users  = new Users();
 		UsersModel usersModel = usersDAO.userLogin(username, password) ;
 		
 		if(usersModel!= null) {
 			BeanUtils.copyProperties(usersModel, users);
-			return ResponseEntity.ok(users);
+			return users;
 			
 		}       
 		else {
-			return ResponseEntity.notFound().build();
+			return null;
 	    }
 		
 	}
